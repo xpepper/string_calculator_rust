73401356562ed56bd5e274449841961306756978
Pietro Di Bello <pierodibello@gmail.com>
2024-12-28 18:15:30 +0100

    feat: have the add function to handle errors and return results
    This commit modifies the `add` function to return a `Result<i32, AddError>`
    instead of a plain integer. It introduces error handling for cases where
    the input string cannot be parsed into a number. The function now returns
    Ok(0) for empty strings and leverages the `AddError` enum to provide
    meaningful error messages for invalid inputs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,49 @@
-pub fn add(numbers: &str) -> i32 {
+use std::num::ParseIntError;
+
+pub fn add(numbers: &str) -> Result<i32, AddError> {
     if numbers.is_empty() {
-        return 0;
+        return Ok(0);
     }
-    1
+
+    let result = numbers.parse()?;
+    Ok(result)
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum AddError {
+    CannotParseNumber(String),
+}
+
+impl From<ParseIntError> for AddError {
+    fn from(value: ParseIntError) -> Self {
+        AddError::CannotParseNumber(value.to_string())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::add;
+    use super::{add, AddError};
 
     #[test]
     fn empty_string_is_0() {
         let empty = "";
-        assert_eq!(0, add(empty));
+        assert_eq!(Ok(0), add(empty));
     }
 
     #[test]
     fn string_with_just_a_number_is_that_number() {
         let just_a_number = "1";
-        assert_eq!(1, add(just_a_number));
+        assert_eq!(Ok(1), add(just_a_number));
+    }
+
+    #[test]
+    fn cannot_add_string_containing_unparsable_numbers() {
+        let not_a_number = "ABC";
+        assert_eq!(
+            Err(AddError::CannotParseNumber(
+                "invalid digit found in string".to_string()
+            )),
+            add(not_a_number)
+        );
     }
 }
