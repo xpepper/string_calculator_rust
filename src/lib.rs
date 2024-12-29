4c0d78f9eb8d5a231277f96d8a3147c83403d095
Pietro Di Bello <pierodibello@gmail.com>
2024-12-29 18:59:27 +0100

    refactor(test): invert expected and actual to follow Rust conventions

--- src/lib.rs
+++ src/lib.rs
@@ -29,29 +29,29 @@ mod tests {
     #[test]
     fn empty_string_is_0() {
         let empty = "";
-        assert_eq!(Ok(0), add(empty));
+        assert_eq!(add(empty), Ok(0));
     }
 
     #[test]
     fn string_with_just_a_number_is_that_number() {
         let just_a_number = "1";
-        assert_eq!(Ok(1), add(just_a_number));
+        assert_eq!(add(just_a_number), Ok(1));
     }
 
     #[test]
     fn sum_two_numbers_separated_by_comma() {
         let two_numbers = "1,2";
-        assert_eq!(Ok(3), add(two_numbers));
+        assert_eq!(add(two_numbers), Ok(3));
     }
 
     #[test]
     fn cannot_add_string_containing_unparsable_numbers() {
         let not_a_number = "ABC";
         assert_eq!(
+            add(not_a_number),
             Err(AddError::CannotParseNumber(
                 "invalid digit found in string".to_string()
-            )),
-            add(not_a_number)
+            ))
         );
     }
 }
