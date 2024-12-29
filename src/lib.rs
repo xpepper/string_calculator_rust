fec8bebde22c0b1239b5398cc3b06a1c4ea5aa6d
Pietro Di Bello <pierodibello@gmail.com>
2024-12-29 23:12:45 +0100

    style: remove comments

--- src/lib.rs
+++ src/lib.rs
@@ -77,11 +77,9 @@ mod tests {
 
     #[test]
     fn support_different_delimiters() {
-        //to change a delimiter, the beginning of the string will contain
-        // a separate line that looks like this: “//[delimiter]\n[numbers…]” for example “//;\n1;2” should return three where the default delimiter is ‘;’ .
-        // the first line is optional. all existing scenarios should still be supported
         assert_eq!(add("//;\n1;2"), Ok(3))
     }
+
     #[test]
     fn cannot_add_string_containing_unparsable_numbers() {
         assert_eq!(
